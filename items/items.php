8b6bf73f025c79b8035d2370b40761428b772c11
Mirna Hasan <mirnamhasan50@gmail.com>
2025-07-14 00:49:34 +0330

    favoriteلmا

--- items/items.php
+++ items/items.php
@@ -1,3 +1,23 @@
 <?php 
 include "../connect.php";
-getAllData("itemsview","1=1");
+
+$categoryid = filterRequest("id") ;
+$userid = filterRequest("userid");
+// getAllData("itemsview","categories_id = $categoryid");
+$stmt = $con->prepare("SELECT items1view.*, 1 AS favorite FROM items1view INNER JOIN favorite ON
+ favorite.favorite_itemsid = items1view.items_id AND favorite.favorite_usersid= $userid AND categories_id = $categoryid
+ UNION ALL SELECT *, 0 AS favorite FROM items1view
+  WHERE categories_id = $categoryid AND
+  items_id != (SELECT items1view.items_id FROM items1view 
+  INNER JOIN favorite ON favorite.favorite_itemsid = items1view.items_id AND favorite.favorite_usersid=14)");
+  
+  $stmt->execute();
+  
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $count  = $stmt->rowCount();
+    if ($count>0){
+        echo json_encode(array("status" => "success" , "data"=> $data)) ;
+
+    }else{
+        echo json_encode(array("status" => "failure ")) ;
+    }
