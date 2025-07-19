11a968290e53b0b3de9e5dec7c4e2de0a3f13e38
Mirna Hasan <mirnamhasan50@gmail.com>
2025-07-19 16:04:23 +0330

    itemsssm

--- items/items.php
+++ items/items.php
@@ -5,10 +5,11 @@ $categoryid = filterRequest("id") ;
 $userid = filterRequest("userid");
 // getAllData("itemsview","categories_id = $categoryid");
 $stmt = $con->prepare("SELECT items1view.*, 1 AS favorite FROM items1view INNER JOIN favorite ON
- favorite.favorite_itemsid = items1view.items_id AND favorite.favorite_usersid= $userid AND categories_id = $categoryid
+ favorite.favorite_itemsid = items1view.items_id AND favorite.favorite_usersid= $userid 
+ AND categories_id = $categoryid
  UNION ALL SELECT *, 0 AS favorite FROM items1view
   WHERE categories_id = $categoryid AND
-  items_id != (SELECT items1view.items_id FROM items1view 
+  items_id NOT IN (SELECT items1view.items_id FROM items1view 
   INNER JOIN favorite ON favorite.favorite_itemsid = items1view.items_id AND favorite.favorite_usersid=14)");
   
   $stmt->execute();
