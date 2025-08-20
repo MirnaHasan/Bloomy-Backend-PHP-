89a16ebbede033dc8d30b7ff4202552e593f52ea
Mirna Hasan <mirnamhasan50@gmail.com>
2025-08-20 20:16:54 +0330

    discount222n

--- view.sql
+++ view.sql
@@ -8,3 +8,10 @@ CREATE OR REPLACE VIEW items1view AS
 SELECT items.* , categories.* FROM items 
 INNER JOIN categories ON items.items_cat = categories.categories_id ;
 
+
+
+CREATE OR REPLACE VIEW cartview AS
+SELECT SUM(items.items_price - items.items_price * items.items_discount/100) as itemsprice , COUNT(cart.cart_itemsid) as countitems , cart.* , items.* FROM cart 
+INNER JOIN items ON items.items_id = cart.cart_itemsid
+GROUP BY cart.cart_itemsid , cart.cart_usersid ;
+
