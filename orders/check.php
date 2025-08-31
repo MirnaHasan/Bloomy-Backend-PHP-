4005c74e5a3b9c3922d4a74fbd5e695888aa0abd
Mirna Hasan <mirnamhasan50@gmail.com>
2025-08-31 15:43:43 +0330

    gotocheckoutbackend-

--- orders/check.php
+++ orders/check.php
@@ -7,6 +7,18 @@ $orderpayment = filterRequest("orderpayment") ;
 $couponid = filterRequest("couponid") ;
 $orderprice = filterRequest("orderprice") ;
 $orderpricedelivery = filterRequest("orderpricedelivery") ;
+$orderprice = filterRequest("orderprice") ;
+$discountcoupon = filterRequest("discountcoupon") ;
+$totalprice = $orderprice + $orderpricedelivery ;
+ // check coupon 
+ $now = date('Y-m-d H:i:s') ;  
+
+ $checkcoupon=getData("coupon" ,"coupon_id = '$couponid' AND coupon_expirdate > '$now' AND coupon_count > 0 " ,null ,false) ;
+if ($checkcoupon > 0){
+    $totalprice =  $totalprice - ($orderprice * $discountcoupon /100); 
+
+}
+
 $data = array (
     "orders_usersid"         => $usersid ,
     "orders_address"         => $addressid , 
@@ -14,7 +26,9 @@ $data = array (
     "orders_paymentmethod"   => $orderpayment  , 
     "orders_coupon"          => $couponid , 
     "orders_price"           => $orderprice ,
+    "orders_totalprice"     => $totalprice ,
     "orders_pricedelivery"   => $orderpricedelivery  ,  
+    "discountcoupon"=> $discountcoupon
 
 ) ; 
  $count =  insertData("orders" , $data , false) ; 
